ee61fa55cdb4d7ca2bc4b67bde351d61d6ddf21c
Yana <iana@me.me>
2019-01-09 18:57:07 +0200

    Changes in UI and sunrise/sunset

--- WeatherApi3.0/Controllers/OpenWeatherMapMvcController.cs
+++ WeatherApi3.0/Controllers/OpenWeatherMapMvcController.cs
@@ -17,23 +17,29 @@ namespace WeatherApi3._0.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-                string APIkey = "6d85a7afd458036f67cfcce6e5c8815f";
-                string LocationSofia = "Sofia";
-                string LocationPlovdiv = "Plovdiv";
-                string LocationBurgas = "Burgas";
-                HttpWebRequest URL = WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?q=" + LocationSofia + "&APPID=" + APIkey + "&units=metric") as HttpWebRequest;
-                string apiResponse = "";
-                 using (HttpWebResponse response = URL.GetResponse() as HttpWebResponse)
-                 {
-                     StreamReader reader = new StreamReader(response.GetResponseStream());
-                     apiResponse = reader.ReadToEnd();
-                 }
+            string APIkey = "6d85a7afd458036f67cfcce6e5c8815f";
+            string LocationSofia = "Sofia";
+            string LocationPlovdiv = "Plovdiv";
+            string LocationBurgas = "Burgas";
+            int LocationSofiaID = 727011;
+            int LocationPlovdivID = 728193;
+            int LocationBurgasID = 732771;
+            // HttpWebRequest URL = WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?q=" + LocationSofia + "&APPID=" + APIkey + "&units=metric") as HttpWebRequest;
+            HttpWebRequest URL = WebRequest.Create("http://api.openweathermap.org/data/2.5/group?id=" + LocationSofiaID + "," + LocationPlovdivID + "," + LocationBurgasID + "&APPID=" + APIkey + "&units=metric") as HttpWebRequest;
+            string apiResponse = "";
+            using (HttpWebResponse response = URL.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                apiResponse = reader.ReadToEnd();
+            }
 
-                ResponseWeather rootObject = JsonConvert.DeserializeObject<ResponseWeather>(apiResponse);
-                return View(rootObject);
+            ResponseWeatherList rootObject = JsonConvert.DeserializeObject<ResponseWeatherList>(apiResponse);
+            return View(rootObject);
         }
+
         [HttpGet]
-        public string Error() {
+        public string Error()
+        {
             string ErrorMessage = "Ops something went wrong";
             return ErrorMessage;
         }
