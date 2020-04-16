1304647c5223bf324f6b26573c0899d3a562ec8e
Boris Dimitrov <boris.d@mail.bg>
2020-04-16 11:55:23 +0300

    Partial view and loading current weather conditions by coordinates

--- WeatherApi3.0/Controllers/OpenWeatherMapMvcController.cs
+++ WeatherApi3.0/Controllers/OpenWeatherMapMvcController.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using System.Net;
+﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using WeatherApiClass;
-using System.Text;
 using WeatherApiModel;
 
 namespace WeatherApi3._0.Controllers
@@ -16,18 +15,20 @@ namespace WeatherApi3._0.Controllers
     public class OpenWeatherMapMvcController : Controller
     {
         private string APIkey = "6d85a7afd458036f67cfcce6e5c8815f";
-
+    
         [HttpGet]
         public IActionResult Index()
-        {            
+        {          
             string LocationSofia = "Sofia";
             string LocationPlovdiv = "Plovdiv";
             string LocationBurgas = "Burgas";
             int LocationSofiaID = 727011;
             int LocationPlovdivID = 728193;
             int LocationBurgasID = 732771;
-            // HttpWebRequest URL = WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?q=" + LocationSofia + "&APPID=" + APIkey + "&units=metric") as HttpWebRequest;
-            HttpWebRequest URL = WebRequest.Create("http://api.openweathermap.org/data/2.5/group?id=" + LocationSofiaID + "&APPID=" + APIkey + "&units=metric" + "&lang=bg") as HttpWebRequest;
+            double latitude = 42.69;
+            double longitude = 23.32;
+            HttpWebRequest URL = WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?q=" + LocationSofia + "&APPID=" + APIkey + "&units=metric" + "lang=bg") as HttpWebRequest;
+            //HttpWebRequest URL = WebRequest.Create("http://api.openweathermap.org/data/2.5/weather" + "?lat=" + latitude + "&lon=" + longitude + "&APPID=" + APIkey + "&units=metric" + "&lang=bg") as HttpWebRequest;
             string apiResponse = "";
             using (HttpWebResponse response = URL.GetResponse() as HttpWebResponse)
             {
@@ -44,24 +45,24 @@ namespace WeatherApi3._0.Controllers
             };
             return View(Model);
         }
-
+        
         [HttpGet]
-        public IActionResult GetCurrentConditionsAndForecast(double latitude, double longitude)
+        public async Task<IActionResult> GetCurrentConditionsAndForecast(double latitude, double longitude)
         {
-            HttpWebRequest URL = WebRequest.Create("http://api.openweathermap.org/data/2.5/group?&lat=" + latitude + "&lon=" + longitude + "&APPID=" + APIkey + "&units=metric" + "&lang=bg") as HttpWebRequest;
-            string apiResponse = "";
-            using (HttpWebResponse response = URL.GetResponse() as HttpWebResponse)
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://api.openweathermap.org");
+            var uri = "http://api.openweathermap.org/data/2.5/weather" + "?lat=" + latitude + "&lon=" + longitude + "&APPID=" + APIkey + "&units=metric" + "&lang=bg";
+            var response = await httpClient.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
             {
-                StreamReader reader = new StreamReader(response.GetResponseStream());
-                apiResponse = reader.ReadToEnd();
+                var content = await response.Content.ReadAsStringAsync();
+                var currentConditions = JsonConvert.DeserializeObject<ResponseWeather>(content);
+                return View("_CurrentLocationConditions", currentConditions);
             }
-
-            ResponseWeatherList rootObject = JsonConvert.DeserializeObject<ResponseWeatherList>(apiResponse);
-            OpenWeatherMap Model = new OpenWeatherMap()
+            else
             {
-                CurrentWeatherConditions = rootObject.list.FirstOrDefault()
-            };
-            return View(Model);
+                return StatusCode((int)response.StatusCode);
+            }
         }
 
         /* http://api.openweathermap.org/data/2.5/forecast?q=Sofia&APPID=6d85a7afd458036f67cfcce6e5c8815f&units=metric */
